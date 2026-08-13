488577331305c27b1afd2f1b375375db9d60c2ed
Jesse Josiah <jessejosiah@MacBook-Pro.local>
2026-08-13 05:49:22 +0100

    Publish StackShift CLI v1.0.4

--- Formula/stackshift.rb
+++ Formula/stackshift.rb
@@ -1,8 +1,8 @@
 class Stackshift < Formula
   desc "Manage StackShift from the command-line"
   homepage "https://stackshift.cloud/"
-  url "https://github.com/stackshiftCloud/stackshift-cli/releases/download/v1.0.3/stackshift-cli-source-v1.0.3.tar.gz"
-  sha256 "d75d7e981800cc48b828814b636b48a20b8dbe5543d507fa6ebcc167d286ca67"
+  url "https://github.com/stackshiftCloud/stackshift-cli/releases/download/v1.0.4/stackshift-cli-source-v1.0.4.tar.gz"
+  sha256 "aab301dbefa15d32870592714e4eba41d5495169ba5bc1ed01bad18a6a6364e6"
   license :cannot_represent
 
   depends_on "go" => :build
@@ -13,8 +13,8 @@ class Stackshift < Formula
     ldflags = %W[
       -s -w
       -X main.version=#{version}
-      -X main.commit=72d99646c145e35bafecdd8c4d0ced06d23fef7e
-      -X main.date=2026-07-31T04:54:40Z
+      -X main.commit=b765f6fcb923dbd41117e2d8a78febeaf7392cb2
+      -X main.date=2026-08-13T04:45:21Z
     ]
     system "go", "build", *std_go_args(ldflags: ldflags.join(" ")),
            "./cmd/stackshift"
